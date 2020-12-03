54b8eb4aeeb80eb00284b6a2cbe3b9ee210ba36a
Ramesh Chandra <nodagalachandra@gmail.com>
2020-12-03 19:00:09 +0530

    Initial commit

--- app.py
+++ app.py
@@ -7,34 +7,61 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-# Replace below path with the absolute path
-# to chrome or Firefox driver in your computer
-driver = webdriver.Firefox()
 
-driver.get("https://web.whatsapp.com/")
-wait = WebDriverWait(driver, 60)
-
-# Replace 'Friend's Name' with the name of your friend
-# or the name of a group
-target = '"Target name"'
-
-x_arg = '//span[contains(@title,' + target + ')]'
-group_title = wait.until(EC.presence_of_element_located((
-    By.XPATH, x_arg)))
-group_title.click()
-inp_xpath = '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
-input_box = wait.until(EC.presence_of_element_located((
-    By.XPATH, inp_xpath)))
-for i in range(10):
+def app(tn, cnt, txt_op, dv=1):
+    if dv == 2:
+        driver = webdriver.Chrome()
+        print("Browser = Chrome")
+    else:
+        print("Browser = Firefox")
+        driver = webdriver.Firefox()
+    driver.get("https://web.whatsapp.com/")
+    wait = WebDriverWait(driver, 60)
+    target = f'"{tn}"'
+    print(f"target = {target}")
+    print(f"sending {txt_op}, {cnt} times")
     emoji = ['😀', '😃', '😄', '😁', '😆', '😅', '😂', '😊', '😇', '🙂', '🙃', '😉', '😌', '😍', '🥰', '😘',
              '😗', '😙', '😚', '😋', '😛', '😝', '😜', '🤪', '🤨', '🧐', '🤓', '😎', '🤩', '🥳', '😏', '😒', '😞', '😔',
              '😟', '😕', '🙁', '☹️', '😣', '😖', '😫', '😩', '🥺', '😢', '😭', '😤', '😠', '😡', '🤬', '🤯', '😳', '🥵',
              '🥶', '😱', '😨', '😰', '😥', '😓', '🤗', '🤭', '🤫', '🤥', '😶', '😐', '😑', '😬', '🙄', '😯', '😦',
              '😧', '😮', '😲', '🥱', '😴', '🤤', '😪', '😵', '🤐', '🥴', '🤢', '🤮', '🤧', '😷', '🤒', '🤕', '🤑', '🤠',
              '😈', '👿']
-    random.shuffle(emoji)
-    e = random.choice(emoji)
-    input_box.send_keys(e)
-    print(f"message sent #{i+1} > {e}")
-    input_box.send_keys(Keys.ENTER)
-    time.sleep(1)
+    x_arg = '//span[contains(@title,' + target + ')]'
+    group_title = wait.until(EC.presence_of_element_located((
+        By.XPATH, x_arg)))
+    group_title.click()
+    inp_xpath = '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
+    input_box = wait.until(EC.presence_of_element_located((
+        By.XPATH, inp_xpath)))
+    for i in range(cnt):
+        random.shuffle(emoji)
+        em = random.choice(emoji)
+        if txt_op.lower() == "emoji":
+            input_box.send_keys(em)
+            print(f"message sent #{i + 1} > {em}")
+            input_box.send_keys(Keys.ENTER)
+            time.sleep(1)
+        else:
+            input_box.send_keys(txt_op)
+            print(f"message sent #{i + 1} > {txt_op}")
+            input_box.send_keys(Keys.ENTER)
+            time.sleep(1)
+
+
+drv = int(input("""
+    1 > Firefox
+    2 > Chrome
+    choose browser: """))
+
+target_name = input("enter the target name or group name: ")
+num = int(input("enter the number of times to send the message: "))
+txt = input(f"""type the text to send (Remember the names are case sensitive
+so type exactly as the contact name...)
+            OR
+type "emoji" to send random emojis ("Chrome browser does not support emojis"): """)
+
+
+try:
+    app(target_name, num, txt, drv)
+except Exception as e:
+    print(e)
